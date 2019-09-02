986e13b521f36123e6dd1a18ea78a8a53de8f2c1
Swetha <swetha.khaidalagudam@generalassemb.ly>
2019-09-02 16:09:10 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,22 @@
-var cardOne = 'queen';
-var cardTwo = 'king';
-var cardThree = 'queen';
-var cardFour = 'king';
-console.log("User flipped " + cardOne);
-console.log("User now flipped " + cardFour);
+
+
+var cards = ["queen" , "queen" , "king" , "king"];
+var cardsInPlay =[];
+
+var cardOne = [cards[0]];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardsInPlay);
+
+var cardTwo = [cards[2]];
+cardsInPlay.push(cardTwo);
+console.log("user flipped " + cardsInPlay);
+
+if(cardsInPlay.length === 2){
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("you found a match!");
+	}
+	else {
+		alert("sorry, try again.");
+	}
+
+}
