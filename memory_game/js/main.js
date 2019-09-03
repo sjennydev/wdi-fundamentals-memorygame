6067b09ff3e57672ef59f886e8f91ec8317e65d9
Swetha <swetha.khaidalagudam@generalassemb.ly>
2019-09-03 17:09:44 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,27 @@
 
 
-var cards = ["queen" , "queen" , "king" , "king"];
+var cards = [
+{ 
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"	
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay =[];
 function checkForMatch() {
 	if(cardsInPlay[0] === cardsInPlay[1]){
@@ -16,8 +37,11 @@ function flipCard(cardId) {
 	
 
 }
-	console.log("User flipped " + cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
 	checkForMatch();
+
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 }
 
 flipCard(1);
